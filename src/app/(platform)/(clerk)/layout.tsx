c91d1022512d387316d01b4e71f064a8f75771c0
noiqum <onurcoskun99@gmail.com>
2024-03-25 21:19:14 +0300

    feat: authentication flow , pages

---
+++ src/app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export default ClerkLayout;
